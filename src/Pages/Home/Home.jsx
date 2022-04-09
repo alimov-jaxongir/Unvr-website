e4739ed17c9dfeb27465bf411a2eba55d2144bdc
Jakhongir <alimovjaxongir1632@gmail.com>
2022-04-09 17:27:53 +0500

    fourth commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import './Home.css'
 import {FcNext} from 'react-icons/fc'
-import top1 from '../../assets/yangiliklar/top1.png'
 import card1 from '../../assets/yangiliklar/card1.png'
 import card2 from '../../assets/yangiliklar/card2.png'
-import bottom1 from '../../assets/yangiliklar/bottom1.png'
 import bcard1 from '../../assets/yangiliklar/bcard1.png'
 import bcard2 from '../../assets/yangiliklar/bcard2.png'
 
@@ -47,14 +45,14 @@ const Home=()=> {
            
           </dir>
           <div className='News__show-bottom bott'>
-               <div className='bcard1'>
-                <img src={card1} alt="card" />
+               <div className='card1'>
+                <img src={bcard1} alt="card" />
                 <div className='info'>
                 <h4 className='title'>2022-yil Universitetimizda yangi fakultetlar ochilmoqda</h4>
                 <div className="description">Vazirlar Mahkamasining «Oliy ta’lim muassasalari pedagog kadrlarini qayta tayyorlash va ularning...</div>
               </div> </div>
-              <div className="bcard1">
-               <img src={card2} alt="card" />
+              <div className="card1">
+               <img src={bcard2} alt="card" />
                <div className='info'>
                 <h4 className='title'>2022-yil Universitetimizda yangi fakultetlar ochilmoqda</h4>
                 <div className="description">Vazirlar Mahkamasining «Oliy ta’lim muassasalari pedagog kadrlarini qayta tayyorlash va ularning...</div>
@@ -107,7 +105,8 @@ const Home=()=> {
           </div>
         </div>
         <div className='Media__show'>
-          
+           
+
         </div>
 
       </div>
